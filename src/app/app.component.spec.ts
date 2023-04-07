ebc73fef06fc000db4798c22ea49aac7cfa39984
Vanshaj <vanshajsharma2019@gmail.com>
2023-04-07 11:25:40 +0530

    Basic

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'FirstApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FirstApp');
+    expect(app).toEqual('FirstApp');
   });
 
   it('should render title', () => {
